d6b45571c95f966070b719acebd36ee0dd916808
Jadelynn Dao <jadelynn@stanford.edu>
2026-01-21 10:59:49 -0800

    Obscure API key in evaluate_vlms.sh
    Obscure the OpenAI API key in the script.

--- evaluate_vlms.sh
+++ evaluate_vlms.sh
@@ -1,6 +1,6 @@
 #bin/bash
 
-export OPENAI_API_KEY="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" # replace with your API key
+export OPENAI_API_KEY="sk-or-v1-...." # replace with your API key
 export OPENAI_BASE_URL="https://openrouter.ai/api/v1"
 
 vlms=("Gemini" "Claude" "GPT4_v") # add more VLMs here
